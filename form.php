5498f26c6e879c0093aadd7187122123fcb2f35a
Mohammed Salah <moeldowy@iti.gov.eg>
2023-12-09 16:21:30 +0200

    from local to github

---
+++ form.php
@@ -0,0 +1,9 @@
+<form method="post" action="#">
+    <label for="username">Username:</label>
+    <input type="text" id="username">
+        <br>
+    <label for="password">Password:</label>
+    <input type="password" id="password">
+    <br>
+<input type="submit" name="Login">
+</form>
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+//Data Types
+/*
+ * scalar data types
+ * string - integer - double - boolean
+ * ----------------------
+ * Compound Data types
+ * array - object
+ * -----------------------
+ * Special Data Types
+ * Null-resource
+ */
+echo "Login Form";
+//require_once "form.php";
+
+
